a6054d48ad6724851cfcf7bae427ecbfc8d95b75
Kirakosyan A <evolutioner.tyoma@yandex.ru>
2022-02-28 09:56:53 +0300

    made a slider

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
+// const swiper = document.querySelector('.swiper').swiper;
 const openSearchBtn = document.getElementById('open-search');
 const searchArea = document.querySelector('.nav__search');
 const navItems = document.querySelector('.nav__items');
 const closeSearchBtn = document.getElementById('close-search-btn');
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    allowTouchMove: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
 
 openSearchBtn.addEventListener('click', () => {
     openSearchBtn.style.display = 'none';
